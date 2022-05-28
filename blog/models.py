f9f15034fca0465638ad8e8321fe096adb29a6cb
edgar84 <edgar.tomko.97@mail.ru>
2022-05-28 13:56:14 +0300

    create views, urls and add get_absolute_url in models

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,13 @@
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 from django.contrib.auth.models import User
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status='published')
+
 
 class Post(models.Model):
     STATUS_CHOICES = (
@@ -21,3 +28,11 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year,
+                                                 self.publish.month, self.publish.day, self.slug])
+
+
+    objects = models.Manager()
+    published = PublishedManager()
