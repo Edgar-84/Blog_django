f7c467d70d511123a98d0f8a4998a475fe6466f0
edgar84 <edgar.tomko.97@mail.ru>
2022-05-29 09:40:02 +0300

    change view post_lish on class PostListView

--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 from .models import Post
-
-
-def post_list(request):
-    object_list = Post.published.all()
-    paginator = Paginator(object_list, 3)
-    page = request.GET.get('page')
-    try:
-        posts = paginator.page(page)
-    except PageNotAnInteger:
-        posts = paginator.page(1)
-    except EmptyPage:
-        posts = paginator.page(paginator.num_pages)
-    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
 
 
 def post_detail(request, year, month, day, post):
@@ -22,4 +10,8 @@ def post_detail(request, year, month, day, post):
     return render(request, 'blog/post/detail.html', {'post': post})
 
 
-
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
