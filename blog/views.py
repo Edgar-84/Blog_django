0577af2b759e88e022d54118442c9d088ab69fcb
edgar84 <edgar.tomko.97@mail.ru>
2022-06-16 06:34:03 +0300

    added search with weighted queries

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,12 @@
-from django.db.models import Count
-from django.core.mail import send_mail
-from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, get_object_or_404
-
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.views.generic import ListView
-from .models import Post, Comment
-from django.contrib.postgres.search import SearchVector
-from .forms import EmailPostForm, CommentForm, SearchForm
+from django.core.mail import send_mail
+from django.db.models import Count
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 from taggit.models import Tag
+from .models import Post, Comment
+from .forms import EmailPostForm, CommentForm, SearchForm
 
 
 def post_search(request):
@@ -16,14 +15,19 @@ def post_search(request):
     results = []
     if 'query' in request.GET:
         form = SearchForm(request.GET)
-    if form.is_valid():
-        query = form.cleaned_data['query']
-        results = Post.objects.annotate(
-            search=SearchVector('title', 'body'),
-        ).filter(search=query)
-    return render(request, 'blog/post/search.html', {'form': form,
-                                                         'query': query,
-                                                         'results': results})
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            search_vector = SearchVector('title', weight='A') + SearchVector('body', weight='B')
+            search_query = SearchQuery(query)
+            results = Post.objects.annotate(
+                rank=SearchRank(search_vector, search_query)
+            ).filter(rank__gte=0.3).order_by('-rank')
+
+    return render(request,
+                  'blog/post/search.html',
+                  {'form': form,
+                   'query': query,
+                   'results': results})
 
 
 def post_list(request, tag_slug=None):
