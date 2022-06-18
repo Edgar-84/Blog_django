3586c9910054868f6666fb509e2d51b23f39b125
edgar84 <edgar.tomko.97@mail.ru>
2022-06-18 14:29:52 +0300

    create add Dockerfile

--- mysite/settings.py
+++ mysite/settings.py
@@ -83,13 +83,24 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'blog',
+#         'USER': 'postgres',
+#         'PASSWORD': os.getenv("PASSWORD_DB"),
+#         'HOST': 'localhost',
+#         'PORT': 5432,
+#     }
+# }
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'blog',
+        'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': os.getenv("PASSWORD_DB"),
-        'HOST': 'localhost',
+        'HOST': 'dm_db',
         'PORT': 5432,
     }
 }
